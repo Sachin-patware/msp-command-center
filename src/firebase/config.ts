091e98f405bba4f892673f461d57ce7a5e78f607
Sachin Patware <sachinpatware10@gmail.com>
2025-11-02 12:22:44 +0000

    // This file is intentionally left blank. // The Firebase configuration

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 // The Firebase configuration will be injected by the build process.
 // Avoid adding any code to this file, as it will be overwritten.
 export const firebaseConfig = {
-  "projectId": "studio-2286212834-f81bf",
-  "appId": "1:235187417153:web:fca44356a6bb9c758fd6c0",
-  "apiKey": "AIzaSyAVy12g0A78X9ryNAk69K2DagFQXj_0M10",
-  "authDomain": "studio-2286212834-f81bf.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "235187417153",
-  "storageBucket": "studio-2286212834-f81bf.appspot.com"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 };
