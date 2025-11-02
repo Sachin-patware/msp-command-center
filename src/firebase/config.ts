9be268a170d57f8790e3cc63766a35c642deb0cc
Sachin Patware <sachinpatware10@gmail.com>
2025-11-02 11:01:27 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,12 @@
 // This file is intentionally left blank. 
 // The Firebase configuration will be injected by the build process.
 // Avoid adding any code to this file, as it will be overwritten.
-export const firebaseConfig = {"apiKey":"API_KEY","authDomain":"PROJECT_ID.firebaseapp.com","projectId":"PROJECT_ID","storageBucket":"PROJECT_ID.appspot.com","messagingSenderId":"SENDER_ID","appId":"APP_ID"};
+export const firebaseConfig = {
+  "projectId": "studio-2286212834-f81bf",
+  "appId": "1:235187417153:web:fca44356a6bb9c758fd6c0",
+  "apiKey": "AIzaSyAVy12g0A78X9ryNAk69K2DagFQXj_0M10",
+  "authDomain": "studio-2286212834-f81bf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "235187417153",
+  "storageBucket": "studio-2286212834-f81bf.appspot.com"
+};
