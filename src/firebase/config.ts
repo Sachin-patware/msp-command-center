4fd4c108aee8b8f629344f225e4d8fc3a737cedb
Sachin Patware <sachinpatware10@gmail.com>
2025-11-02 10:59:39 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,4 @@
 // This file is intentionally left blank. 
 // The Firebase configuration will be injected by the build process.
 // Avoid adding any code to this file, as it will be overwritten.
-export const firebaseConfig = {};
+export const firebaseConfig = {"apiKey":"API_KEY","authDomain":"PROJECT_ID.firebaseapp.com","projectId":"PROJECT_ID","storageBucket":"PROJECT_ID.appspot.com","messagingSenderId":"SENDER_ID","appId":"APP_ID"};
